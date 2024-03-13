ddd36f3885cec23a20aeb1fcb2c77d819a78d164
adelphevianney <fofvianney15@gmail.com>
2024-03-13 13:16:16 +0000

    mise en ligne du site xeb azertype

---
+++ script/config.js
@@ -0,0 +1,4 @@
+//les listes ou composants du jeu
+
+const listeMots=["cachalot" , "Pétunia" , "Serviette"]
+const listePhrases=["Pas de panique !" , "La vie, l’univers et le reste" , "Merci pour le poisson"]
---
+++ script/main.js
@@ -0,0 +1,15 @@
+lancerJeu()
+//inputEcriture=document.getElementById("inputEcriture")
+//console.log(inputEcriture)
+
+//btnValiderMot= document.getElementById("btnValiderMot")
+//console.log(btnValiderMot)
+
+//btnRadioChoix= document.querySelectorAll(".optionSource input")
+//console.log(btnRadioChoix)
+
+//zoneSaisie= document.querySelector(".zoneSaisie input")
+//console.log(zoneSaisie)
+
+//zoneScore= document.querySelector(".zoneScore span")
+//console.log(zoneScore)
